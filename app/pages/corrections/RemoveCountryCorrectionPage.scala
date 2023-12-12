e8eaae54149c4526ce562b95c788b98b0791adc2
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2023-12-12 15:26:14 +0000

    VEIOSS-398 Added RemoveCountryCorrectionController

---
+++ app/pages/corrections/RemoveCountryCorrectionPage.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pages.corrections
+
+import models.{Index, UserAnswers}
+import pages.{Page, QuestionPage, Waypoints}
+import play.api.libs.json.JsPath
+import play.api.mvc.Call
+
+case class RemoveCountryCorrectionPage(periodIndex: Index, countryIndex: Index) extends QuestionPage[Boolean] {
+
+  override def path: JsPath = JsPath \ toString
+
+  override def toString: String = "removeCountryCorrection"
+
+  override def route(waypoints: Waypoints): Call = controllers.corrections.routes.RemoveCountryCorrectionController.onPageLoad(waypoints, periodIndex, countryIndex)
+
+  override protected def nextPageNormalMode(waypoints: Waypoints, answers: UserAnswers): Page =
+    CorrectPreviousReturnPage
+
+
+}
