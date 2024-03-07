983c57989384f7b154b3c7acc12c6bfc4e3ad43a
DevAndyroo <andy@andyroo.info>
2024-03-07 16:52:03 +0000

    Refactor missing tests

--- test/controllers/StartOutstandingReturnControllerSpec.scala
+++ test/controllers/StartOutstandingReturnControllerSpec.scala
@@ -17,43 +17,80 @@
 package controllers
 
 import base.SpecBase
+import connectors.ReturnStatusConnector
+import models.SubmissionStatus
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{reset, when}
+import org.scalatest.BeforeAndAfterEach
+import org.scalatestplus.mockito.MockitoSugar
+import pages.EmptyWaypoints
+import play.api.inject.bind
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import views.html.NoReturnsDueView
+import utils.FutureSyntax.FutureOps
+import viewmodels.yourAccount.{CurrentReturns, Return}
 
-class StartOutstandingReturnControllerSpec extends SpecBase {
+class StartOutstandingReturnControllerSpec extends SpecBase with MockitoSugar with BeforeAndAfterEach {
+
+  private val mockReturnStatusConnector = mock[ReturnStatusConnector]
+
+  override protected def beforeEach(): Unit = {
+    reset(mockReturnStatusConnector)
+  }
 
   "StartOutstandingReturn Controller" - {
 
     "must redirect to the first due return" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      when(mockReturnStatusConnector.getCurrentReturns(any())(any())) thenReturn
+        Right(CurrentReturns(
+          Seq(Return(
+            period = period,
+            firstDay = period.firstDay,
+            lastDay = period.lastDay,
+            dueDate = period.paymentDeadline,
+            submissionStatus = SubmissionStatus.Due,
+            inProgress = false,
+            isOldest = true
+          )),
+          excluded = false,
+          finalReturnsCompleted = false
+        )).toFuture
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        .overrides(bind[ReturnStatusConnector].toInstance(mockReturnStatusConnector))
+        .build()
 
       running(application) {
-        val request = FakeRequest(GET, routes.NoReturnsDueController.onPageLoad().url)
+        val request = FakeRequest(GET, routes.StartOutstandingReturnController.onPageLoad().url)
 
         val result = route(application, request).value
 
-        val view = application.injector.instanceOf[NoReturnsDueView]
-
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view()(request, messages(application)).toString
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result) mustBe Some(routes.StartReturnController.onPageLoad(EmptyWaypoints, period).url)
       }
     }
 
     "must redirect to no outstanding returns when none are due return" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      when(mockReturnStatusConnector.getCurrentReturns(any())(any())) thenReturn
+        Right(CurrentReturns(
+          Seq.empty,
+          excluded = false,
+          finalReturnsCompleted = false
+        )).toFuture
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        .overrides(bind[ReturnStatusConnector].toInstance(mockReturnStatusConnector))
+        .build()
 
       running(application) {
-        val request = FakeRequest(GET, routes.NoReturnsDueController.onPageLoad().url)
+        val request = FakeRequest(GET, routes.StartOutstandingReturnController.onPageLoad().url)
 
         val result = route(application, request).value
 
-        val view = application.injector.instanceOf[NoReturnsDueView]
-
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view()(request, messages(application)).toString
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result) mustBe Some(routes.NoReturnsDueController.onPageLoad().url)
       }
     }
   }
