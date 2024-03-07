a0ea6f31598754589a631dc6260d6a1c6118c2b5
Vincent Munier <vincent.munier@digital.hmrc.gov.uk>
2024-03-07 18:59:17 +0000

    Add previousIossNumber to makePayment

--- app/controllers/payments/PaymentController.scala
+++ app/controllers/payments/PaymentController.scala
@@ -19,32 +19,51 @@ package controllers.payments
 import config.Service
 import controllers.actions._
 import models.Period
-import pages.Waypoints
+import pages.{JourneyRecoveryPage, Waypoints}
 import play.api.Configuration
 import play.api.i18n.I18nSupport
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import services.PaymentsService
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
+import services.{PaymentsService, PreviousRegistrationService}
+import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 
 import javax.inject.Inject
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class PaymentController @Inject()(
                                    cc: AuthenticatedControllerComponents,
                                    config: Configuration,
                                    paymentsService: PaymentsService,
+                                   previousRegistrationService: PreviousRegistrationService
                                  )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
   protected val controllerComponents: MessagesControllerComponents = cc
   private val paymentsBaseUrl: Service = config.get[Service]("microservice.services.pay-api")
 
-  def makePayment(waypoints: Waypoints, period: Period, amountInPence: Long): Action[AnyContent] = cc.authAndGetOptionalData().async {
-    implicit request =>
-      val amountOwed = BigDecimal(amountInPence) / 100
+  def makePayment(waypoints: Waypoints, period: Period, amountInPence: Long, previousIossNumber: Option[String]): Action[AnyContent] =
+    cc.authAndGetOptionalData().async { implicit request =>
+      withIossNumber(waypoints, request.iossNumber, previousIossNumber) { iossNumber =>
+        val amountOwed = BigDecimal(amountInPence) / 100
 
-      paymentsService.makePayment(request.iossNumber, period, amountOwed).map {
-        case Right(value) => Redirect(value.nextUrl)
-        case _ => Redirect(s"$paymentsBaseUrl/pay/service-unavailable")
+        paymentsService.makePayment(iossNumber, period, amountOwed).map {
+          case Right(value) => Redirect(value.nextUrl)
+          case _ => Redirect(s"$paymentsBaseUrl/pay/service-unavailable")
+        }
       }
+    }
+
+  private def withIossNumber(waypoints: Waypoints, requestIossNumber: String, previousIossNumber: Option[String])
+                            (f: String => Future[Result])(implicit hc: HeaderCarrier): Future[Result] = {
+    previousIossNumber match {
+      case Some(iossNumber) =>
+        previousRegistrationService.getPreviousRegistrations().flatMap { previousRegistrations =>
+          if (previousRegistrations.exists(_.iossNumber == iossNumber)) {
+            f(iossNumber)
+          } else {
+            Future.successful(Redirect(JourneyRecoveryPage.route(waypoints)))
+          }
+        }
+      case _ => f(requestIossNumber)
+    }
   }
 }
