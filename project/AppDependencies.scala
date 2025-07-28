2e0496d1d8f402692efded8d98e77ebfdf801c9b
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2025-07-28 17:33:34 +0100

    VEOSS-2117 | Update dependencies and plugins as per PR bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,8 +7,8 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"             % "12.6.0",
-    "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30"  % "3.2.0",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc-play-30"             % "12.7.0",
+    "uk.gov.hmrc"         %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
     "uk.gov.hmrc"         %% "domain-play-30"                         % "10.0.0",
@@ -21,7 +21,6 @@ object AppDependencies {
     "org.scalatest"         %% "scalatest"                % "3.2.19",
     "org.scalatestplus"     %% "scalacheck-1-15"          % "3.2.11.0",
     "org.scalacheck"        %% "scalacheck"               % "1.18.1",
-    "org.pegdown"           % "pegdown"                   % "1.6.0",
     "org.jsoup"             % "jsoup"                     % "1.21.1",
     "com.vladsch.flexmark"  % "flexmark-all"              % "0.64.8"
   ).map(_ % "test, it")
