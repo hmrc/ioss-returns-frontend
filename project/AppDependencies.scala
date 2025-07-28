19972123c75ca119d68b497312234ecb1f0149f7
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2025-07-28 17:21:20 +0100

    VEOSS-2117 | Fix Bobby rule enforcement error

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
-  private val hmrcMongoVersion = "2.6.0"
+  private val bootstrapVersion = "9.18.0"
+  private val hmrcMongoVersion = "2.7.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
@@ -12,7 +12,7 @@ object AppDependencies {
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
     "uk.gov.hmrc"         %% "domain-play-30"                         % "10.0.0",
-    "org.typelevel"       %% "cats-core"                              % "2.12.0"
+    "org.typelevel"       %% "cats-core"                              % "2.13.0"
   )
 
   val test = Seq(
@@ -20,9 +20,9 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-test-play-30"  % hmrcMongoVersion,
     "org.scalatest"         %% "scalatest"                % "3.2.19",
     "org.scalatestplus"     %% "scalacheck-1-15"          % "3.2.11.0",
-    "org.scalacheck"        %% "scalacheck"               % "1.18.0",
+    "org.scalacheck"        %% "scalacheck"               % "1.18.1",
     "org.pegdown"           % "pegdown"                   % "1.6.0",
-    "org.jsoup"             % "jsoup"                     % "1.18.1",
+    "org.jsoup"             % "jsoup"                     % "1.21.1",
     "com.vladsch.flexmark"  % "flexmark-all"              % "0.64.8"
   ).map(_ % "test, it")
 
