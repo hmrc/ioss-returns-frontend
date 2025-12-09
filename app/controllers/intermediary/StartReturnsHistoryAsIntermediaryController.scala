5495bd585bc614253ba3d232bcaf54fbe6f99da5
Tayamul <100432502+Tayamul@users.noreply.github.com>
2025-12-09 11:01:36 +0000

    VEI-694: Implement `StartReturnsHistoryAsIntermediaryController`.

---
+++ app/controllers/intermediary/StartReturnsHistoryAsIntermediaryController.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.intermediary
+
+import config.FrontendAppConfig
+import controllers.actions.AuthenticatedControllerComponents
+import controllers.routes
+import models.IntermediarySelectedIossNumber
+import pages.Waypoints
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.IntermediarySelectedIossNumberRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import utils.FutureSyntax.FutureOps
+
+import java.time.Clock
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class StartReturnsHistoryAsIntermediaryController @Inject()(
+                                                           override val messagesApi: MessagesApi,
+                                                           cc: AuthenticatedControllerComponents,
+                                                           intermediarySelectedIossNumberRepository: IntermediarySelectedIossNumberRepository,
+                                                           config: FrontendAppConfig,
+                                                           clock: Clock
+                                                           )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  protected val controllerComponents: MessagesControllerComponents = cc
+  
+  def startReturnsHistoryAsIntermediary(waypoints: Waypoints, iossNumber: String): Action[AnyContent] = (
+    cc.authAndIntermediaryRequired(iossNumber) andThen cc.getData()).async { implicit request =>
+    
+    if (config.intermediaryEnabled) {
+
+      val userAnswers = request.userAnswers.get
+      
+      val intermediaryNumber = request.intermediaryNumber.get
+
+      val intermediarySelectedIossNumber = IntermediarySelectedIossNumber(request.userId, intermediaryNumber, iossNumber)
+      
+      for {
+        _ <- intermediarySelectedIossNumberRepository.set(intermediarySelectedIossNumber)
+        _ <- cc.sessionRepository.set(userAnswers)
+      } yield {
+        Redirect(controllers.previousReturns.routes.SubmittedReturnsHistoryController.onPageLoad(waypoints).url)
+      }
+    } else {
+      Redirect(routes.NotRegisteredController.onPageLoad()).toFuture
+    }
+  }
+}
