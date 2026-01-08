242081159e20a38b026cf8c36142962a71f746a5
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2026-01-08 15:39:04 +0000

    Update Copyrights to 2026.

--- app/controllers/NoOtherPeriodsAvailableController.scala
+++ app/controllers/NoOtherPeriodsAvailableController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,7 +45,7 @@ class NoOtherPeriodsAvailableController @Inject()(
       } else {
         YourAccountPage.route(waypoints).url
       }
-      
+
       Ok(view(waypoints, request.isIntermediary, companyName, redirectUrl))
   }
 }
