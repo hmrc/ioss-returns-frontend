c484e4e118d20d86d3d95f06ab0058d2f9fdf400
Andrew Smith <1895818+DevAndyroo@users.noreply.github.com>
2026-04-30 09:25:28 +0100

    VEOSS-2175 encrypt user answers on authenticated session (#235)

--- it/repositories/SessionRepositorySpec.scala
+++ it/repositories/SessionRepositorySpec.scala
@@ -1,7 +1,10 @@
 package repositories
 
+import com.typesafe.config.Config
 import config.FrontendAppConfig
-import models.{Period, StandardPeriod, UserAnswers}
+import crypto.UserAnswersEncryptor
+import models.{EncryptedUserAnswers, Period, StandardPeriod, UserAnswers}
+import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.mongodb.scala.model.Filters
 import org.scalatest.OptionValues
@@ -9,7 +12,9 @@ import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatestplus.mockito.MockitoSugar
+import play.api.Configuration
 import play.api.libs.json.Json
+import services.crypto.EncryptionService
 import uk.gov.hmrc.mongo.test.DefaultPlayMongoRepositorySupport
 
 import java.time.{Clock, Instant, Month, ZoneId}
@@ -19,7 +24,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 class SessionRepositorySpec
   extends AnyFreeSpec
     with Matchers
-    with DefaultPlayMongoRepositorySupport[UserAnswers]
+    with DefaultPlayMongoRepositorySupport[EncryptedUserAnswers]
     with ScalaFutures
     with IntegrationPatience
     with OptionValues
@@ -34,23 +39,35 @@ class SessionRepositorySpec
   private val mockAppConfig = mock[FrontendAppConfig]
   when(mockAppConfig.cacheTtl) thenReturn 1L
 
+  private val mockConfiguration = mock[Configuration]
+  private val mockConfig = mock[Config]
+  private val mockEncryptionService: EncryptionService = new EncryptionService(mockConfiguration)
+  private val encryptor = new UserAnswersEncryptor(mockAppConfig, mockEncryptionService)
+  private val secretKey: String = "VqmXp7yigDFxbCUdDdNZVIvbW6RgPNJsliv6swQNCL8="
+
   protected override val repository: SessionRepository = new SessionRepository(
     mongoComponent = mongoComponent,
     appConfig = mockAppConfig,
+    encryptor = encryptor,
     clock = stubClock
   )
+
+  when(mockConfiguration.underlying) thenReturn mockConfig
+  when(mockConfig.getString(any())) thenReturn secretKey
+  when(mockAppConfig.encryptionKey) thenReturn secretKey
 
   ".set" - {
 
     "must set the last updated time on the supplied user answers to `now`, and save them" in {
 
       val expectedResult = userAnswers copy (lastUpdated = instant)
+      val encryptedExpectedResult = encryptor.encryptUserAnswers(expectedResult)
 
       val setResult     = repository.set(userAnswers).futureValue
       val updatedRecord = find(Filters.equal("userId", userAnswers.userId)).futureValue.headOption.value
 
       setResult mustEqual true
-      updatedRecord mustEqual expectedResult
+      updatedRecord mustEqual encryptedExpectedResult
     }
   }
 
@@ -60,7 +77,7 @@ class SessionRepositorySpec
 
       "must update the lastUpdated time and get the record" in {
 
-        insert(userAnswers).futureValue
+        insert(encryptor.encryptUserAnswers(userAnswers)).futureValue
 
         val result         = repository.get(userAnswers.userId, userAnswers.iossNumber).futureValue
         val expectedResult = userAnswers copy (lastUpdated = instant)
@@ -83,7 +100,7 @@ class SessionRepositorySpec
 
     "must remove a record" in {
 
-      insert(userAnswers).futureValue
+      insert(encryptor.encryptUserAnswers(userAnswers)).futureValue
 
       val result = repository.clear(userAnswers.userId, userAnswers.iossNumber).futureValue
 
@@ -106,15 +123,16 @@ class SessionRepositorySpec
 
       "must update its lastUpdated to `now` and return true" in {
 
-        insert(userAnswers).futureValue
+        insert(encryptor.encryptUserAnswers(userAnswers)).futureValue
 
         val result = repository.keepAlive(userAnswers.userId, userAnswers.iossNumber).futureValue
 
         val expectedUpdatedAnswers = userAnswers copy (lastUpdated = instant)
+        val encryptedExpectedUpdatedAnswers = encryptor.encryptUserAnswers(expectedUpdatedAnswers)
 
         result mustEqual true
         val updatedAnswers = find(Filters.equal("userId", userAnswers.userId)).futureValue.headOption.value
-        updatedAnswers mustEqual expectedUpdatedAnswers
+        updatedAnswers mustEqual encryptedExpectedUpdatedAnswers
       }
     }
 
