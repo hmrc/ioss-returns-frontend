0e3c5d351cdf6285de38d34095fc981a7db65319
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2026-07-29 07:57:05 +0100

    VEIOSS-915 | Build: Registration Reminder Messaging

---
+++ app/controllers/InterceptReviewUpdateRegistrationController.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.FrontendAppConfig
+import controllers.actions.*
+import pages.{StartReturnPage, Waypoints}
+
+import javax.inject.Inject
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.InterceptReviewUpdateRegistrationView
+
+import scala.concurrent.ExecutionContext
+
+class InterceptReviewUpdateRegistrationController @Inject()(
+                                       override val messagesApi: MessagesApi,
+                                       cc: AuthenticatedControllerComponents,
+                                       frontendAppConfig: FrontendAppConfig,
+                                       view: InterceptReviewUpdateRegistrationView
+                                     )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  protected val controllerComponents: MessagesControllerComponents = cc
+  
+  def onPageLoad(waypoints: Waypoints, iossNumber: String): Action[AnyContent] = cc.authAndRequireData(iossNumber) {
+    implicit request =>
+
+      val period = request.userAnswers.period
+      val changeRegistrationUrl = getChangeRegistrationUrl(request.isIntermediary, iossNumber)
+      val continueUrl = StartReturnPage(iossNumber, period, frontendAppConfig).navigate(waypoints, request.userAnswers, request.userAnswers).route.url
+
+      Ok(view(waypoints, changeRegistrationUrl, continueUrl))
+  }
+
+  private def getChangeRegistrationUrl(isIntermediary: Boolean, iossNumber: String): String = {
+
+    if (isIntermediary) {
+      s"${frontendAppConfig.changeNetpRegistrationUrl}/$iossNumber"
+    } else {
+      frontendAppConfig.amendRegistrationUrl
+    }
+  }
+}
---
+++ test/controllers/InterceptReviewUpdateRegistrationControllerSpec.scala
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import config.FrontendAppConfig
+import pages.StartReturnPage
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import views.html.InterceptReviewUpdateRegistrationView
+
+class InterceptReviewUpdateRegistrationControllerSpec extends SpecBase {
+
+  "InterceptReviewUpdateRegistration Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.InterceptReviewUpdateRegistrationController.onPageLoad(waypoints, iossNumber).url)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[InterceptReviewUpdateRegistrationView]
+        val appConfig = application.injector.instanceOf[FrontendAppConfig]
+        val changeRegistrationUrl = appConfig.amendRegistrationUrl
+        val continueUrl = StartReturnPage(iossNumber, period, appConfig).navigate(waypoints, emptyUserAnswers, emptyUserAnswers).route.url
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(waypoints, changeRegistrationUrl, continueUrl)(request, messages(application)).toString
+      }
+    }
+
+    "must use the NETP registration URL for an intermediary" in {
+
+      val intermediaryNumber = "IN9001234567"
+
+      val application = applicationBuilder(
+        userAnswers = Some(emptyUserAnswers),
+        maybeIntermediaryNumber = Some(intermediaryNumber)
+      ).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.InterceptReviewUpdateRegistrationController.onPageLoad(waypoints, iossNumber).url)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[InterceptReviewUpdateRegistrationView]
+
+        val appConfig = application.injector.instanceOf[FrontendAppConfig]
+
+        val changeRegistrationUrl = s"${appConfig.changeNetpRegistrationUrl}/$iossNumber"
+
+        val continueUrl = StartReturnPage(iossNumber, period, appConfig).navigate(waypoints, emptyUserAnswers, emptyUserAnswers).route.url
+
+        status(result) mustEqual OK
+
+        contentAsString(result) mustEqual view(waypoints, changeRegistrationUrl, continueUrl)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery when no answers are found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.InterceptReviewUpdateRegistrationController.onPageLoad(waypoints, iossNumber).url)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
